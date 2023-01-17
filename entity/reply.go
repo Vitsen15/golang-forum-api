6222a6c4f3b2cc3c8963b4fe72bba27cc13782e7
Andrey Vitsentovich <vitsentovich.andrey@gmail.com>
2023-01-17 16:34:41 +0200

    Clean entities from redundant tags.

--- entity/reply.go
+++ entity/reply.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Reply struct {
-	ID        uint           `gorm:"primarykey" json:"id"`
+	ID        uint           `gorm:"primarykey"`
 	ThreadID  uint           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" faker:"-"`
 	Thread    Thread         `json:"-" faker:"-"`
 	UserID    uint           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" faker:"-"`
@@ -14,5 +14,5 @@ type Reply struct {
 	Body      string         `gorm:"type:text" faker:"paragraph"`
 	CreatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP" faker:"-"`
 	UpdatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP" faker:"-"`
-	DeletedAt gorm.DeletedAt `gorm:"index" faker:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" faker:"-"`
 }
--- entity/thread.go
+++ entity/thread.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Thread struct {
-	ID        uint           `gorm:"primarykey" json:"ID" faker:"-"`
+	ID        uint           `gorm:"primarykey" faker:"-"`
 	UserID    uint           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"UserID,string" binding:"required" faker:"-"`
 	User      User           `json:"-" faker:"-"`
 	Replies   []Reply        `json:"-" faker:"-"`
-	Title     string         `gorm:"size:255" json:"Title" binding:"required" faker:"sentence"`
-	Body      string         `gorm:"type:text" json:"Body" binding:"required" faker:"paragraph"`
-	CreatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP" json:"-" faker:"-"`
-	UpdatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP" json:"-" faker:"-"`
+	Title     string         `gorm:"size:255" binding:"required" faker:"sentence"`
+	Body      string         `gorm:"type:text" binding:"required" faker:"paragraph"`
+	CreatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP" faker:"-"`
+	UpdatedAt time.Time      `gorm:"default:CURRENT_TIMESTAMP" faker:"-"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" faker:"-"`
 }
