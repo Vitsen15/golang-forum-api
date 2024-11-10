90546b571780eb1e49de86243e16726235747946
Andrey Vitsentovich <vitsentovich.andrey@gmail.com>
2024-11-10 11:03:23 +0200

    style: corrected code-style

--- security/password.go
+++ security/password.go
@@ -12,10 +12,9 @@ func HashPassword(password string) (string, error) {
 	return string(hashedPasswordBytes), err
 }
 
-// DoPasswordsMatch Check if two passwords match using Bcrypt CompareHashAndPassword
+// DoPasswordsMatch Check if two passwords match using Bcrypt CompareHashAndPassword,
 // which return nil on success and an error on failure.
 func DoPasswordsMatch(hashedPassword string, currPassword string) bool {
-	err := bcrypt.CompareHashAndPassword(
-		[]byte(hashedPassword), []byte(currPassword))
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(currPassword))
 	return err == nil
 }
